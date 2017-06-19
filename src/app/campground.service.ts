613e50478df081fbf3dc011aa9656a2a31b0a593
Tyler Stephenson <tyler@>
2017-06-19 16:39:45 -0700

    add camp api as service

--- src/app/campground.service.ts
+++ src/app/campground.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+
 
 @Injectable()
 export class CampgroundService {
 
-  constructor() { }
+  constructor (private http: Http) {}
 
+  ngOnInit() {
+  }
+
+  getCampsApi(url) {
+    return this.http.get(url).map(res => {
+        return res;
+      }).subscribe(data => {
+      });
+  }
 }
