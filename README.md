38d648d69da95bee1436eeadd359701065cd13d8
Susha Dore <susha.dore@gmail.com>
2017-06-20 14:57:55 -0700

    enable google sign in for user authentication

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.1.2.
 
+# STRETCH GOALS
+  * user karma rating
+  * add dispensaries
+  * add hot springs
+  * weather API
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,20 +3,30 @@ import { User } from '../user.model';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { UserService } from '../user.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
+  providers: [UserService]
 })
-export class UserComponent {
-
+export class UserComponent implements OnInit {
+  users: FirebaseListObservable<any[]>;
   user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private userService: UserService) {
     this.user = afAuth.authState;
   }
 
+  ngOnInit() {
+    this.users = this.userService.getUsers();
+  }
+  submitForm(name: string, photo: string, email: string, password: string, karma: number) {
+    const newUser: User = new User(name, photo, email, password, karma);
+    this.userService.addUser(newUser);
+  }
   login() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
