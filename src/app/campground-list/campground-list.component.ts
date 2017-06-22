e5904957b6a3aaa829388fb102c6cb06598b2f49
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-06-22 11:24:26 -0700

    pipe, style

--- src/app/campground-list/campground-list.component.ts
+++ src/app/campground-list/campground-list.component.ts
@@ -10,6 +10,7 @@ import { campgroundConfig } from './../api-keys';
   providers: [CampgroundService]
 })
 export class CampgroundListComponent implements OnInit {
+  filterByAmenities: string = "Laundry";
   apiDetailUrl: string;
   apiUrlSearch = `http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=45.5231&landmarkLong=-122.6765&xml=true&api_key=${campgroundConfig.apiKey}`;
   camps;
@@ -32,4 +33,7 @@ export class CampgroundListComponent implements OnInit {
     });
   }
 
+  onChange(optionFromMenu) {
+  this.filterByAmenities = optionFromMenu;
+}
 }
